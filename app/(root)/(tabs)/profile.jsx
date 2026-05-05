6591db29e1884a447d53d9f727c307dc327fa666
hari <ganambihai10@gmail.com>
2026-05-05 16:18:38 +0530

    tabs initiated

---
+++ app/(root)/(tabs)/profile.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+export default function Profile() {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>Profile</Text>
+    </View>
+    </SafeAreaView>
+  )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,5 @@
-import { Stack } from "expo-router";
-import { TextInput ,View,Text,TouchableOpacity} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import "../global.css"
+import { Slot } from "expo-router";
+import "../global.css";
 export default function RootLayout() {
-  return (
-<SafeAreaView style={{flex:1,padding:16 ,backgroundColor:"#fff"}}>
-  <View style={{padding:16}}>
-    <Text>Welcome to my app</Text>
-    <TextInput  placeholder="Search here.." placeholderTextColor="#999" style={{borderWidth:1,borderColor:"#ddd",borderRadius:8,padding:10,marginTop:12}}/>
-    <TouchableOpacity
-    onPress={()=>alert("searching..")}
-    className="bg-red-200 p-4"
-    >
-      <Text style={{color:"#fff",fontWeight:"bold"}}>Search</Text>
-
-    </TouchableOpacity>
-  </View>
-</SafeAreaView>
-
-  );
+  return <Slot />;
 }
