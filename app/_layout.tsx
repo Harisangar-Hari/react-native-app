1d79d7df6793fdad1ef02343cd7a52efa392e726
hari <ganambihai10@gmail.com>
2026-05-05 15:03:34 +0530

    nativewind added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Stack } from "expo-router";
-
+import { TextInput ,View,Text,TouchableOpacity} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css"
 export default function RootLayout() {
-  return <Stack />;
+  return (
+<SafeAreaView style={{flex:1,padding:16 ,backgroundColor:"#fff"}}>
+  <View style={{padding:16}}>
+    <Text>Welcome to my app</Text>
+    <TextInput  placeholder="Search here.." placeholderTextColor="#999" style={{borderWidth:1,borderColor:"#ddd",borderRadius:8,padding:10,marginTop:12}}/>
+    <TouchableOpacity
+    onPress={()=>alert("searching..")}
+    className="bg-red-200 p-4"
+    >
+      <Text style={{color:"#fff",fontWeight:"bold"}}>Search</Text>
+
+    </TouchableOpacity>
+  </View>
+</SafeAreaView>
+
+  );
 }
